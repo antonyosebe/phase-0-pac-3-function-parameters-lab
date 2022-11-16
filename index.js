a36dd1f7c2aa3c3ed56f94231db7b02a72fef839
antonyosebe <osebe.anto@gmail.com>
2022-11-16 22:09:06 +0300

    <completed assignment>

---
+++ index.js
@@ -0,0 +1,13 @@
+function introduction(Aki) {
+    return `Hi, my name is ${Aki}.`;
+}
+function introductionWithLanguage(Aki, Ember) {
+    return `Hi, my name is ${Aki} and I am learning to program in ${Ember}.`;
+}
+function introductionWithLanguageOptional(Gracie, language = "Python") {
+    return `Hi, my name is ${Gracie} and I am learning to program in ${language}.`;
+}
+function introductionWithLanguageOptional(Gracie, language = "JavaScript") {
+    return `Hi, my name is ${Gracie} and I am learning to program in ${language}.`;
+
+}
